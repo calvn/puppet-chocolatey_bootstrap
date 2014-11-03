bbab40350208911ed5fd07499c871f4b0079d3a8
Calvin Leung Huang <cleung2010@gmail.com>
2014-11-03 15:36:45 -0500

    Added extra options

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,9 +1,10 @@
 class chocolatey_bootstrap (
-  $script_url = "https://chocolatey.org/install.ps1"
+  $script_url    = "https://chocolatey.org/install.ps1",
+  $extra_options = "",
 ) {
   exec { "Install chocolatey":
     creates  => 'C:\ProgramData\chocolatey',
-    command  => "iex ((new-object net.webclient).DownloadString(\'${script_url}\'))",
+    command  => "iex ((new-object net.webclient).DownloadString(\'${script_url}\')) $extra_options",
     provider => powershell
   }
 }
